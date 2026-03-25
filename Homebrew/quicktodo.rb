3add4f4daff78070517adab90665c06dfdc83422
yoonho <soldier4443@gmail.com>
2026-03-25 16:33:35 +0900

    Update cask for v0.1.2

--- Homebrew/quicktodo.rb
+++ Homebrew/quicktodo.rb
@@ -1,6 +1,6 @@
 cask "quicktodo" do
-  version "0.1.0"
-  sha256 "4d932e82b5c34ff62d8e22247f7c79b476afbdee88830e0c306bc6e8f244db16"
+  version "0.1.2"
+  sha256 "57b7b940f4a797f638e7089b748fb19117c3ba0d4356652906796053df4c98af"
 
   url "https://github.com/turastory/quicktodo/releases/download/v#{version}/QuickTodo.zip"
   name "QuickTodo"
